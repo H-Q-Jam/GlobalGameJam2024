0a9265bcad699841bc58c59a750d34905b9c63c5
FrlHugo <forel.hugo@gmail.com>
2024-01-24 20:42:19 +0100

    setup photon j'y arrive pas aled

---
+++ Assets/_Scripts/Network/NetworkInputData.cs
@@ -0,0 +1,11 @@
+using Fusion;
+using Fusion.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct NetworkInputData : INetworkInput
+{
+    public Vector3 direction;
+
+}
