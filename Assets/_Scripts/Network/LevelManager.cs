4994813cfd6aa127d329a5f63fa5a9eed0c816cf
FrlHugo <forel.hugo@gmail.com>
2024-01-27 13:06:58 +0100

    amelioration UI + debut multi

---
+++ Assets/_Scripts/Network/LevelManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+
+public class LevelManager : NetworkSceneManagerDefault
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
